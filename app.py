84e3c818251fe2e353e14f83a94dbde362fbf63b
Sam Segal <samsegal@sams-mbp.lan>
2023-03-30 19:43:45 -0600

    added disclaimer

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__, static_folder='templates')
 def home():
     return render_template("index.html")
 
+@app.route("/disclaimer")
+def dis():
+    return render_template("disclaimer.html")
+    
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8000))
     app.run(debug=True, host='0.0.0.0', port=port)
