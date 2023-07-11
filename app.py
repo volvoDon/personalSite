b0a9a98c7df597c23a06509b42bcde641374a284
sam <segalcsam@gmail.com>
2023-07-11 16:37:40 -0600

    init

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask , render_template
 import os
 
-app = Flask(__name__, static_folder='templates')
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/disclaimer")
-def dis():
-    return render_template("disclaimer.html")
+@app.route('/test_image')
+def test_image():
+    return app.send_static_file('Gradient.png')
+
     
 
 if __name__ == "__main__":
